4df9cc9bc2f8ae66565ccbbad0e4a03895f07bdc
Jan Eike Suchard <jan@suchard.cloud>
2023-10-16 15:40:21 +0200

    adding function head of information routes

---
+++ routes/info.go
@@ -0,0 +1,9 @@
+package routes
+
+import "net/http"
+
+// InformationRoute allows users to check the capabilities and available scripts
+// and identifiers for the different algorithms
+func InformationRoute(w http.ResponseWriter, r *http.Request) {
+
+}
