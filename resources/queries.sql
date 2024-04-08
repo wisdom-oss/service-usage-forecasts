3284e7edd341da98f66995f9ee70c22f8056f88a
Jan Eike Suchard <jan@suchard.cloud>
2024-04-08 10:29:12 +0200

    adding bucket versions of the queries
    the bucketed versions automatically sum up the used water amount

--- resources/queries.sql
+++ resources/queries.sql
@@ -29,3 +29,24 @@ SELECT municipality,
 FROM wisdom.timeseries.water_usage
 WHERE municipality ~ $1
   AND usage_type IN ($2);
+
+-- name: get-bucketed-usages-by-municipality
+SELECT municipality,
+    time_bucket($1, time) AS time,
+    usage_type,
+    SUM(amount)           AS amount
+FROM wisdom.timeseries.water_usage
+WHERE municipality ~ $2
+GROUP BY time, municipality, usage_type
+ORDER BY time;
+
+-- name: get-bucketed-usages-by-municipality-consumer-groups
+SELECT municipality,
+    time_bucket($1, time) AS time,
+    usage_type,
+    SUM(amount)           AS amount
+FROM wisdom.timeseries.water_usage
+WHERE municipality ~ $2
+  AND usage_type IN ($3)
+GROUP BY time, municipality, usage_type
+ORDER BY time;
