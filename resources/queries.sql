fecf1e34f2cd7251f50d55a5daa9363a026d6d2f
Jan Eike Suchard <jan@suchard.cloud>
2024-04-08 10:27:13 +0200

    reformatting query file

--- resources/queries.sql
+++ resources/queries.sql
@@ -1,31 +1,31 @@
 -- name: get-consumer-groups
 SELECT id,
-       name,
-       description,
-       external_identifier
+    name,
+    description,
+    external_identifier
 FROM wisdom.water_usage.usage_types;
 
 -- name: get-consumer-groups-by-external-id
 SELECT id,
-       name,
-       description,
-       external_identifier
+    name,
+    description,
+    external_identifier
 FROM wisdom.water_usage.usage_types
 WHERE external_identifier = ANY ($1);
 
 -- name: get-usages-by-municipality
 SELECT municipality,
-       time,
-       usage_type,
-       amount
+    time,
+    usage_type,
+    amount
 FROM wisdom.timeseries.water_usage
 WHERE municipality ~ $1;
 
 -- name: get-usages-by-municipality-consumer-groups
 SELECT municipality,
-       time,
-       usage_type,
-       amount
+    time,
+    usage_type,
+    amount
 FROM wisdom.timeseries.water_usage
 WHERE municipality ~ $1
   AND usage_type IN ($2);
