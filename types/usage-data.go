0efa2e22fb1e8597145509de2d3af542df078d22
Jan Eike Suchard <jan@suchard.cloud>
2024-04-08 09:50:35 +0200

    updating datatype

--- types/usage-data.go
+++ types/usage-data.go
@@ -1,10 +1,12 @@
 package types
 
-import "time"
+import (
+	"github.com/jackc/pgx/v5/pgtype"
+)
 
 type UsageDataPoint struct {
-	Municipal string    `json:"municipal" db:"municipality"`
-	UsageType string    `json:"usageType" db:"usage_type"`
-	Date      time.Time `json:"date" db:"date"`
-	Amount    float64   `json:"amount" db:"amount"`
+	Municipal string             `json:"municipal" db:"municipality"`
+	UsageType pgtype.UUID        `json:"usageType" db:"usage_type"`
+	Date      pgtype.Timestamptz `json:"date" db:"time"`
+	Amount    float64            `json:"amount" db:"amount"`
 }
