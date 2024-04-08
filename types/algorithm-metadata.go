b13f2c9576cf2bd7357ff4d4f397e9a654df0b37
Jan Eike Suchard <jan@suchard.cloud>
2024-04-08 10:29:33 +0200

    extending algorithm metadata to allow specification of bucket size

--- types/algorithm-metadata.go
+++ types/algorithm-metadata.go
@@ -7,4 +7,12 @@ type AlgorithmMetadata struct {
 	// Parameters is an array that contains all parameters that may be passed to
 	// the algorithm using the request body
 	Parameters map[string]Parameter `json:"parameters"`
+
+	// UseBuckets specifies if the data query uses buckets of time to
+	// pre-aggregate the data it receives
+	UseBuckets bool `json:"useBuckets" yaml:"useBuckets"`
+
+	// BucketSize specifies the size of each bucket in seconds.
+	// As the buckets may only be of a positive size, the values here are uint64
+	BucketSize uint64 `json:"bucketSize" yaml:"bucketSize"`
 }
